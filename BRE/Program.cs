b5df652edd3fd04d92e4a9fe60646de1e1a299aa
alex529 <savin.alexandru@live.com>
2020-10-14 03:13:45 +0200

    implement messenger

---
+++ BRE/Program.cs
@@ -0,0 +1,83 @@
+﻿using BRE.Handlers;
+using System;
+using System.Collections.Generic;
+
+namespace BRE
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Setup(Messenger.Instance, new EmailService());
+
+            //msg.Publish(new PhysicalProductPayment());
+            //msg.Publish(new BookPayment());
+            //var mem = new MembershipPayment();
+            //msg.Publish(mem);
+            //Thread.Sleep(500); // as the events are run concurently a sall delay needs to be added to allow membershipRepo to be populated 
+
+            //msg.Publish(new UpgradeMembershipPayment() { MebershipID = mem.MebershipID });
+            //msg.Publish(new VideoPayment());
+            //msg.Publish(new VideoPayment() { VideoName = "Learning to Ski" });
+            //Thread.Sleep(1000); //wait untill the task have processed
+        }
+
+        /// <summary>
+        /// This is a "mock" function, as both the payments and handlers are very light, and i don't get it from the req where to register the event processors they are wrapped in this function here.
+        /// The main point of the function is to show that IMessenger needs to be injected in the place where the "processors" are going to be registered.
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void Setup(IMessenger msg, IEmailService mail)
+        {
+            var membershipRepo = new Dictionary<Guid, Membership>();//this should be injected as well
+
+            msg.Subscribe<PhysicalProductPayment>(p =>
+            {
+                var slip = new PackingSlip();
+                slip.Products.Add(p.Payload.ProductID);
+                slip.Generate();
+
+                CommissionPaymentGenerator(p.Payload.Total, p.Payload.AgentID);
+            });
+
+            msg.Subscribe<BookPayment>(b =>
+            {
+                var slip = new PackingSlip();
+                slip.Products.Add(b.Payload.ProductID);
+                slip.Departments.Add("royality");
+                slip.Generate();
+
+                CommissionPaymentGenerator(b.Payload.Total, b.Payload.AgentID);
+            });
+
+            msg.Subscribe<MembershipPayment>(m =>
+            {
+                var ms = new Membership(m.Payload.MebershipID) { Email = m.Payload.Email };
+                ms.Activate();
+                membershipRepo[ms.ID] = ms;
+
+                mail.SendActivationEmail(m.Payload.Email);
+            });
+
+            msg.Subscribe<UpgradeMembershipPayment>(m =>
+            {
+                membershipRepo[m.Payload.MebershipID].Upgrade();
+
+                mail.SendUpgradeEmail(membershipRepo[m.Payload.MebershipID].Email);
+            });
+
+            msg.Subscribe<VideoPayment>(v =>
+            {
+                var slip = new PackingSlip();
+                slip.Products.Add(v.Payload.ProductID);
+                if (v.Payload.VideoName == "Learning to Ski")
+                    slip.Products.Add(ProductNameToProductID("First Aid"));
+
+                slip.Generate();
+            });
+        }
+
+        private static Guid ProductNameToProductID(string name) => Guid.NewGuid();
+        private static void CommissionPaymentGenerator(int total, Guid agentID) => Console.WriteLine($"Agent: {agentID} receives: {0.01 * total}");
+    }
+}
---
+++ BRETest/ProgramTest.cs
@@ -0,0 +1,45 @@
+﻿using BRE;
+using BRE.Handlers;
+using Moq;
+using System.Threading;
+using Xunit;
+
+namespace BRETest
+{
+    public class ProgramTest
+    {
+        //[Fact]
+        //public void Setup_AllPaymentsHaveHandlers()
+        //{
+        //    var types = new List<Type>();
+        //    var mockMessenger = new Mock<IMessenger>();
+        //    mockMessenger.Setup(m => m.Subscribe(It.IsAny<Action<Message<It.IsAnyType>>>())).Callback((x)=>  types.Add(x.GetType()));
+
+        //    Program.Setup(mockMessenger.Object, new EmailService());
+
+        //    mockMessenger.Verify(m => m.Subscribe(It.IsAny<Action<Message<It.IsAnyType>>>()), Times.Exactly(5));
+        //    //CHECK IF ALL THE TYPES ARE PROPERLY INSERTED in types
+        //} this test is not properly done, it should be possible to achieve a bit more research is needed in how generic methods need to be setup 
+
+
+        /// <summary>
+        /// Using this technique of injecting dependencies we can test mot of the code, 
+        /// the same kind of design can be implemented for CommissionPaymentGenerator, and for PackingSlip, the generate function ca be injected and afterward it can be mocked in tests.
+        /// </summary>
+        [Fact]
+        public void Setup_SendEmails()
+        {
+            var emailMock = new Mock<IEmailService>();
+            Program.Setup(Messenger.Instance, emailMock.Object);
+
+            var mem = new MembershipPayment();
+            Messenger.Instance.Publish(mem);
+            Thread.Sleep(500); //this should no be added to tests, a better way of finding out when the task are done should be implemented inside of the Messenger class
+            Messenger.Instance.Publish(new UpgradeMembershipPayment() { MebershipID = mem.MebershipID });
+            Thread.Sleep(500);
+
+            emailMock.Verify(e => e.SendActivationEmail(It.IsAny<string>()), Times.Once);
+            emailMock.Verify(e => e.SendUpgradeEmail(It.IsAny<string>()), Times.Once);
+        }
+    }
+}
